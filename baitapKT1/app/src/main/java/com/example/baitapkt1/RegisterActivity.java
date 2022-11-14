da2b526808a1de805f9a0d9899682319afa29e7f
PNP0972 <2050531200254@sv.ute.udn.vn>
2022-11-15 02:06:06 +0700

    firebase

---
+++ baitapKT1/app/src/main/java/com/example/baitapkt1/RegisterActivity.java
@@ -0,0 +1,103 @@
+package com.example.baitapkt1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+    TextView alreadyHaveAccount;
+
+    EditText inputEmail,inputPassword,inputConformPassword;
+    Button btnRegister;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        alreadyHaveAccount=findViewById(R.id.alreadyHaveAccount);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        inputEmail=findViewById(R.id.inputEmail);
+        inputPassword=findViewById(R.id.inputPassword);
+        inputConformPassword=findViewById(R.id.inputConformPassword);
+        btnRegister=findViewById(R.id.btnRegister);
+        progressDialog= new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
+
+
+        alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PerforAuth();
+            }
+        });
+    }
+
+    private void PerforAuth() {
+        String email=inputEmail.getText().toString();
+        String password=inputPassword.getText().toString();
+        String confrimPassword=inputConformPassword.getText().toString();
+
+        if(!email.matches(emailPattern)){
+            inputEmail.setError( "Enter Connext Email");
+        }else if(password.isEmpty()|| password.length()<6){
+            inputPassword.setError("Enter Proper Password");
+        }else if(!password.equals(confrimPassword)){
+            inputConformPassword.setError("Password Not match Both field");
+        }else {
+            progressDialog.setMessage("Please Wait While Registrantion...");
+            progressDialog.setTitle("Registrantion");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        progressDialog.dismiss();
+                        sendUserToNextAcitivity();
+                        Toast.makeText(RegisterActivity.this, "Registaration Successful", Toast.LENGTH_SHORT).show();
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToNextAcitivity() {
+        Intent intent   = new Intent(RegisterActivity.this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
